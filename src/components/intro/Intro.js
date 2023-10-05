d4931d1afce5ce92526c05ea4ee01d558c278c3f
Xempii <gartoonqqq@gmail.com>
2023-10-05 22:09:48 +0700

    Fix Btn to estimate

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,30 +1,30 @@
 import React from "react";
 import "./Intro.css"; // Import the CSS file for this component
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Intro() {
-  return (
-    <div className="IntroContainer">
-      <div className="space-intro">
-        <h1 className="IntroTitle">PREFABVOLUTION</h1>
-        <h1 className="IntroTitle">EXTENDING</h1>
-        <h1 className="IntroTitle">INNOVATION.</h1>
-        <br />
-  
-        <p className="IntroContent">ต่อเติมบ้านอย่างมีคุณภาพด้วย PrefabVolution</p>
-        <p className="IntroContent">ผลิตจากอุตสาหกรรม ลดปัญหางานก่อสร้าง</p>
-        <p className="IntroContent">ติดตั้งไว จบงานเร็วสุด 14 วัน</p>
+   return (
+      <div className="IntroContainer">
+         <div className="space-intro">
+            <h1 className="IntroTitle">PREFABVOLUTION</h1>
+            <h1 className="IntroTitle">EXTENDING</h1>
+            <h1 className="IntroTitle">INNOVATION.</h1>
+            <br />
 
-        <br />
+            <p className="IntroContent">
+               ต่อเติมบ้านอย่างมีคุณภาพด้วย PrefabVolution
+            </p>
+            <p className="IntroContent">ผลิตจากอุตสาหกรรม ลดปัญหางานก่อสร้าง</p>
+            <p className="IntroContent">ติดตั้งไว จบงานเร็วสุด 14 วัน</p>
 
-        <button className="free_estimate">
-          <Link to="/" >
-              ประเมินราคา ฟรี
-            </Link>
-        </button>
+            <br />
+
+            <button className="free_estimate">
+               <Link to="/estimate">ประเมินราคา ฟรี</Link>
+            </button>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default Intro;
