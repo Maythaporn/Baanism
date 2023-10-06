a4c034f8f8786621b0239e2d5d5bdb01036a261a
Xempii <gartoonqqq@gmail.com>
2023-10-06 20:22:03 +0700

    Add responsive estimate page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,57 +1,57 @@
-import logo from './../../assets/images/LOGO_Linear_BGW_FTB.png';
-import './Header.css';
-import { Link, useNavigate } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
+import logo from "./../../assets/images/LOGO_Linear_BGW_FTB.png";
+import "./Header.css";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 export default function Header() {
-  const navigate = useNavigate();
-  const [isNavOpen, setIsNavOpen] = useState(false);
+   const navigate = useNavigate();
+   const [isNavOpen, setIsNavOpen] = useState(false);
 
-  const toggleNav = () => {
-    setIsNavOpen(!isNavOpen);
-  };
+   const toggleNav = () => {
+      setIsNavOpen(!isNavOpen);
+   };
 
-  const btnRegister = () => {
-    navigate('/register');
-  };
+   const btnRegister = () => {
+      navigate("/register");
+   };
 
-  const btnLogin = () => {
-    navigate('/login');
-  };
+   const btnLogin = () => {
+      navigate("/login");
+   };
 
-  const closeNav = () => {
-    setIsNavOpen(false);
-  };
+   const closeNav = () => {
+      setIsNavOpen(false);
+   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 769) {
-        closeNav(); // Close the navigation when the screen size is 769px or more
-      }
-    };
+   useEffect(() => {
+      const handleResize = () => {
+         if (window.innerWidth >= 769) {
+            closeNav(); // Close the navigation when the screen size is 769px or more
+         }
+      };
 
-    window.addEventListener('resize', handleResize);
+      window.addEventListener("resize", handleResize);
 
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+      // Clean up the event listener when the component unmounts
+      return () => {
+         window.removeEventListener("resize", handleResize);
+      };
+   }, []);
 
-  return (
-    <>
-      <nav className={`navbar ${isNavOpen ? 'active' : ''}`}>
-        <Link to="/">
-          <img src={logo} alt="baanism-logo" className="logoStyleF" />
-        </Link>
+   return (
+      <>
+         <nav className={`navbar ${isNavOpen ? "active" : ""}`}>
+            <Link to="/">
+               <img src={logo} alt="baanism-logo" className="logoStyleF" />
+            </Link>
 
-        <div className="nav-toggle" onClick={toggleNav}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
+            <div className="nav-toggle" onClick={toggleNav}>
+               <div className="bar"></div>
+               <div className="bar"></div>
+               <div className="bar"></div>
+            </div>
 
-        {/* <div className='left-side'>
+            {/* <div className='left-side'>
         <Link to="/">
           <img src={logo} alt="baanism-logo" className="logoStyle" />
         </Link>
@@ -79,45 +79,37 @@ export default function Header() {
           </ul>
         </div> */}
 
-        <div className={`nav-links ${isNavOpen ? 'open' : ''}`}>
-          <ul className='navBar'>
-            <li>
-              <Link to="/about">
-                เกี่ยวกับเรา
-              </Link>
-            </li>
-            <li>
-              <Link to='/homecontent'>
-                Home GURU
-              </Link>
-            </li>
-            <li>
-              <Link to='/'>
-                ประเมินราคา
-              </Link>
-            </li>
-            <li>
-              <Link to='/'>
-                ติดต่อเรา
-              </Link>
-            </li>
-            <li>
-              <button className='lineButton'>
-                <Link onClick={btnLogin} to='/login'>
-                  ลงชื่อเข้าใช้งาน
-                </Link>
-              </button>
-            </li>
-            <li>
-              <button className='solidButton'>
-                <Link onClick={btnRegister} to='/Register'>
-                  ลงทะเบียน
-                </Link>
-              </button>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
-  );
+            <div className={`nav-links ${isNavOpen ? "open" : ""}`}>
+               <ul className="navBar">
+                  <li>
+                     <Link to="/about">เกี่ยวกับเรา</Link>
+                  </li>
+                  <li>
+                     <Link to="/homecontent">Home GURU</Link>
+                  </li>
+                  <li>
+                     <Link to="/estimate">ประเมินราคา</Link>
+                  </li>
+                  <li>
+                     <Link to="/">ติดต่อเรา</Link>
+                  </li>
+                  <li>
+                     <button className="lineButton">
+                        <Link onClick={btnLogin} to="/login">
+                           ลงชื่อเข้าใช้งาน
+                        </Link>
+                     </button>
+                  </li>
+                  <li>
+                     <button className="solidButton">
+                        <Link onClick={btnRegister} to="/Register">
+                           ลงทะเบียน
+                        </Link>
+                     </button>
+                  </li>
+               </ul>
+            </div>
+         </nav>
+      </>
+   );
 }
