1e65b30e3a093b36d3b3669fb5142e31d8c8e086
moomim57kiki <moomim57@gmail.com>
2023-10-14 18:21:26 +0700

    fix domain

--- src/components/Admin/header/Admin-header.js
+++ src/components/Admin/header/Admin-header.js
@@ -1,92 +1,65 @@
-import logo from './logo-header.png';
-import './Header.css';
-import { Link, useNavigate } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import {FaBell,FaUser} from 'react-icons/fa'
+import logo from "./logo-header.png";
+import "./Header.css";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { FaBell, FaUser } from "react-icons/fa";
 
 export default function Header() {
-  const navigate = useNavigate();
-  const [isNavOpen, setIsNavOpen] = useState(false);
 
-  const toggleNav = () => {
-    setIsNavOpen(!isNavOpen);
-  };
-
-  const btnRegister = () => {
-    navigate('/register');
-  };
-
-  const btnLogin = () => {
-    navigate('/login');
-  };
-
-  const closeNav = () => {
-    setIsNavOpen(false);
-  };
 
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 769) {
-        closeNav(); // Close the navigation when the screen size is 769px or more
+        // closeNav(); // Close the navigation when the screen size is 769px or more
       }
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     // Clean up the event listener when the component unmounts
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
     <>
-      <nav className={`navbar ${isNavOpen ? 'active' : ''}`}>
+           <div className="UserHeader">
         <Link to="/">
-          <img src={logo} alt="baanism-logo" className="logoStyle" />
+          <img src={logo} alt="baanism-logo" className="UserHeaderLogo" />
         </Link>
 
-        <div className="nav-toggle" onClick={toggleNav}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
+        <div className="mim-container">
+          <Link to="/about">เกี่ยวกับเรา</Link>
+          <hr
+            style={{
+              width: "40px",
+            }}
+          ></hr>
+          <Link to="/homecontent">Home GURU Content</Link>
+          <hr
+            style={{
+              width: "40px",
+            }}
+          ></hr>
+          <Link to="/">ประเมินราคา</Link>
+          <hr
+            style={{
+              width: "40px",
+            }}
+          ></hr>
+          <Link to="/">ติดต่อเรา</Link>
         </div>
-
-        <div className={`nav-links ${isNavOpen ? 'open' : ''}`}>
-          <ul className='navBar'>
-            <li>
-              <Link to="/about">
-                เกี่ยวกับเรา
-              </Link>
-            </li>
-            <li>
-              <Link to='/homecontent'>
-                Home GURU Content
-              </Link>
-            </li>
-            <li>
-              <Link to='/'>
-                ประเมินราคา
-              </Link>
-            </li>
-            <li>
-              <Link to='/'>
-                ติดต่อเรา
-              </Link>
-            </li>
-            <li>
-              <Link>
-              <FaBell size={20} color="gray" />
-              </Link>
-            </li>
-            <li>
-              <Link>
-              <FaUser size={20} color="gray" />
-              </Link>
-            </li>
-          </ul>
+        <div className="mim-container2">
+          <Link>
+            <FaBell size={30} color="gray" />
+          </Link>
+          {"        "}
+          <Link>
+            <FaUser size={30} color="gray" />
+          </Link>
         </div>
-      </nav>
+      </div>
     </>
   );
 }
