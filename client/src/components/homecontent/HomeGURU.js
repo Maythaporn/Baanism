98e55d7fe679ec4e9e8fe277d2f51da4e15bf9a0
xEARTHx <61792750+xEARTHx@users.noreply.github.com>
2023-10-29 14:51:00 +0700

    homecontent paginate

--- client/src/components/homecontent/HomeGURU.js
+++ client/src/components/homecontent/HomeGURU.js
@@ -1,38 +1,27 @@
 import React, { useEffect, useState } from "react";
 import "./HomeGURU.css";
 import Axios from 'axios'
-import { useNavigate } from "react-router-dom";
+import Paginate from "./Paginate";
 
 function Intro() {
   const [content, setContent] = useState([])
-  const navigate = useNavigate();
 
   useEffect(() => {
     Axios.get('http://localhost:3001/homecontent')
       .then((response) => {
-        setContent(response.data);
+        setContent(response.data)
       })
       .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+        console.error(error)
+      })
+  }, [])
 
   return (
     <div className="HomeGURUContainer">
       <h2 className="HomeGURUContent">ความรู้เรื่องต่อเติมบ้าน</h2>
-      <div className="content">
-        {content.map((val) => {
-          return (
-            <div className="content-info" key={val.id} onClick={() => { navigate(`/homecontent/${val.id}`) }}>
-              <img src={val.img} alt={val.title} />
-              <h3>{val.title}</h3>
-              <pre>{val.caption}</pre>
-            </div>
-          )
-        })}
-      </div>
+      <Paginate data={content} />
     </div>
   );
 }
 
-export default Intro;
+export default Intro
