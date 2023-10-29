36985482721a4ef3fdd380b09020cfbbda1a6c5c
xEARTHx <61792750+xEARTHx@users.noreply.github.com>
2023-10-29 23:51:38 +0700

    delete content

--- client/src/components/Admin/update-project/update.js
+++ client/src/components/Admin/update-project/update.js
@@ -3,8 +3,8 @@ import './update.css'
 import Axios from 'axios'
 
 const UpdateProjects = () => {
-    const [data ,setData] = useState([])
-
+    const [data, setData] = useState([])
+    const [confirm, setConfirm] = useState();
     // const data = [
     //     { id: 1, title: "ไม้ฝา เฌอร่า คืออะไร?" },
     //     { id: 2, title: "ไม้ฝา เฌอร่า คืออะไร?" },
@@ -12,14 +12,25 @@ const UpdateProjects = () => {
 
     useEffect(() => {
         Axios.get('http://localhost:3001/homecontent')
-          .then((response) => {
-            setData(response.data)
-            console.log(response.data)
-          })
-          .catch((error) => {
-            console.error(error)
-          })
-      }, [])
+            .then((response) => {
+                setData(response.data)
+                // console.log(response.data)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }, [])
+
+    const deleteContent = (id) => {
+        Axios.delete(`http://localhost:3001/deletecontent/${id}`).then((response) => {
+            setData(
+                data.filter((val) => {
+                    return val.id != id
+                })
+            )
+            setConfirm()
+        })
+    }
 
     return (
         <>
@@ -32,11 +43,20 @@ const UpdateProjects = () => {
                         <div className='edit'>
                             <button className='edit-btn'>แก้ไขข้อมูล</button>
                             <span className='space'>|</span>
-                            <button className='edit-btn'>ลบ Content</button>
+                            <button className='edit-btn' onClick={() => { setConfirm(e.id) }}>ลบ Content</button>
                         </div>
                     </div>
                 </div>
             ))}
+            {confirm && (
+                <div className="confirmation-popup">
+                    <p>คุณต้องการลบใช่หรือไม่?</p>
+                    <div className="btn">
+                        <button onClick={() => deleteContent(confirm)}  className="y">ใช่</button>
+                        <button onClick={() => setConfirm() } className="n">ยกเลิก</button>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
